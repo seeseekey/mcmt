1aba4301123bbc231d06140af365516c7d233e98
seeseekey <seeseekey@googlemail.com>
2012-01-16 10:22:26 +0100

    Implement repairBedrockLayer.

--- mcmt/Program.cs
+++ mcmt/Program.cs
@@ -19,6 +19,9 @@
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 using System;
+using System.Collections.Generic;
+using CSCL;
+using Substrate;
 
 namespace mcmt
 {
@@ -29,14 +32,14 @@ namespace mcmt
 			Console.WriteLine("mcmt v1.00");
 			Console.WriteLine("(c) 2012 by the seeseekey (http://seeseekey.net)");
 			Console.WriteLine("");
-			Console.WriteLine("Nutzung: ivktool -action -parameters");
-			Console.WriteLine("  z.B. mcmt -repairBedrockLayer world");
+			Console.WriteLine("Nutzung: mcmt -action -parameters");
+			Console.WriteLine("  z.B. mcmt -repairBedrockLayer D:\\world");
 			Console.WriteLine("");
-			Console.WriteLine("  -repairBedrockLayer <mapfile>");
+			Console.WriteLine("  -repairBedrockLayer <worldPath>");
 			Console.WriteLine("  -replace (not implemented)");
 		}
 		
-				static List<string> GetFilesFromParameters(Parameters param)
+		static List<string> GetFilesFromParameters(Parameters param)
 		{
 			List<string> ret=new List<string>();
 
@@ -52,51 +55,47 @@ namespace mcmt
 		}
 		
 		#region Functions
-		private void RepairBedrockLayer(string world)
+		private static void RepairBedrockLayer(string dest)
 		{
-		if (args.Length != 3) {
-                Console.WriteLine("Usage: BlockReplace <world> <before-id> <after-id>");
-                return;
-            }
+			// Open our world
+			BetaWorld world=BetaWorld.Open(dest);
 
-            string dest = args[0];
-            int before = Convert.ToInt32(args[1]);
-            int after = Convert.ToInt32(args[2]);
+			// The chunk manager is more efficient than the block manager for
+			// this purpose, since we'll inspect every block
+			BetaChunkManager cm=world.GetChunkManager();
 
-            // Open our world
-            BetaWorld world = BetaWorld.Open(dest);
+			foreach(ChunkRef chunk in cm)
+			{
+				Console.WriteLine("Process chunk: {0}/{1}", chunk.X, chunk.Z);
 
-            // The chunk manager is more efficient than the block manager for
-            // this purpose, since we'll inspect every block
-            BetaChunkManager cm = world.GetChunkManager();
+				// You could hardcode your dimensions, but maybe some day they
+				// won't always be 16.  Also the CLR is a bit stupid and has
+				// trouble optimizing repeated calls to Chunk.Blocks.xx, so we
+				// cache them in locals
+				int xdim=chunk.Blocks.XDim;
+				int ydim=chunk.Blocks.YDim;
+				int zdim=chunk.Blocks.ZDim;
 
-            foreach (ChunkRef chunk in cm) {
-                // You could hardcode your dimensions, but maybe some day they
-                // won't always be 16.  Also the CLR is a bit stupid and has
-                // trouble optimizing repeated calls to Chunk.Blocks.xx, so we
-                // cache them in locals
-                int xdim = chunk.Blocks.XDim;
-                int ydim = chunk.Blocks.YDim;
-                int zdim = chunk.Blocks.ZDim;
+				// x, z, y is the most efficient order to scan blocks (not that
+				// you should care about internal detail)
+				for(int x=0; x<xdim; x++)
+				{
+					for(int z=0; z<zdim; z++)
+					{
+						// Replace the block with after if it matches before
+						if(chunk.Blocks.GetID(x, 0, z)!=BlockType.BEDROCK)
+						{
+							chunk.Blocks.SetData(x, 0, z, 0);
+							chunk.Blocks.SetID(x, 0, z, BlockType.BEDROCK);
 
-                // x, z, y is the most efficient order to scan blocks (not that
-                // you should care about internal detail)
-                for (int x = 0; x < xdim; x++) {
-                    for (int z = 0; z < zdim; z++) {
-                        for (int y = 0; y < ydim; y++) {
+							Console.WriteLine("Repair Bedrock layer on {0}/{1} in chunk {2}/{3}", x, z, chunk.X, chunk.Z);
+						}
+					}
+				}
 
-                            // Replace the block with after if it matches before
-                            if (chunk.Blocks.GetID(x, y, z) == before) {
-                                chunk.Blocks.SetData(x, y, z, 0);
-                                chunk.Blocks.SetID(x, y, z, after);
-                            }
-                        }
-                    }
-                }
-
-                // Save the chunk
-                cm.Save();
-            }
+				// Save the chunk
+				cm.Save();
+			}
 		}
 		#endregion
 		
